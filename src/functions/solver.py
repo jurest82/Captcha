a2748109a0c8630875e227ba62e939339623c253
jurest82 <52606768+jurest82@users.noreply.github.com>
2023-01-22 12:09:53 -0500

    feat: save Textract response in S3

--- src/functions/solver.py
+++ src/functions/solver.py
@@ -7,13 +7,15 @@ def handler(event, context):
     bucket_name = event['Records'][0]['s3']['bucket']['name']
     document = event['Records'][0]['s3']['object']['key']
 
-    client = boto3.client('textract', region_name=region)
-    response = client.detect_document_text(
+    textract_client = boto3.client('textract', region_name=region)
+    response = textract_client.detect_document_text(
         Document={'S3Object': {
             'Bucket': bucket_name,
             'Name': document
         }})
 
-    print(response['Blocks'][1]['Text'])
+    with open('/tmp/solution.txt', 'w') as f:
+        f.write(f'''{response['Blocks'][1]['Text']}''')
 
-    return response['Blocks'][1]['Text']
+    s3 = boto3.client("s3")
+    s3.upload_file('/tmp/solution.txt', bucket_name, 'solutions/solution.txt')
