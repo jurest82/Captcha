7ed2c0ae6e1c6f012eb449ddc00b511080292eab
jurest82 <52606768+jurest82@users.noreply.github.com>
2023-01-22 12:11:41 -0500

    chore: remove unused libraries from entrypoint

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -12,15 +12,5 @@ pip3 install -r requirements.txt -t $PYTHON_LAYER_DIST_PACKAGES_PATH
 echo "/home/volume/src/library/$PYTHON_LAYER_DIST_PACKAGES_PATH" > captcha-solver-library.pth
 sudo cp captcha-solver-library.pth $PYTHON_DIST_PACKAGES_PATH
 
-cd $EXEC_PATH/../src/layers/commons
-pip3 install -r requirements.txt -t $PYTHON_LAYER_DIST_PACKAGES_PATH
-echo "/home/volume/src/layers/commons/$PYTHON_LAYER_DIST_PACKAGES_PATH" > captcha-solver-layers-commons.pth
-sudo cp captcha-solver-layers-commons.pth $PYTHON_DIST_PACKAGES_PATH
-
-cd $EXEC_PATH/../src/layers/pandas
-pip3 install -r requirements.txt -t $PYTHON_LAYER_DIST_PACKAGES_PATH
-echo "/home/volume/src/layers/pandas/$PYTHON_LAYER_DIST_PACKAGES_PATH" > captcha-solver-layers-pandas.pth
-sudo cp captcha-solver-layers-pandas.pth $PYTHON_DIST_PACKAGES_PATH
-
 sudo chmod +x $EXEC_PATH/aws.sh
 $EXEC_PATH/aws.sh
